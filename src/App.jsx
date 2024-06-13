980f572705283804ac3f9b94671dae54f2c09485
ParsaAghaali <92843932+ParsaAghaali@users.noreply.github.com>
2024-06-13 17:37:22 +0330

    fix(SearchUsers): dependancy fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import './App.css';
 import Login from './components/login';
 import Signup from './components/signup';
 import Error from './components/error';
-import EditProfile from './components/Edituser';
+// import EditProfile from './components/Edituser';
 import Landing from './pages/Landing';
 import { AuthProvider } from './context/AuthContext';
 import SearchPaginationUsers from './pages/SearchPaginationUsers';
 
-import Crud from './components/CRUD/Crud';
+// import Crud from './components/CRUD/Crud';
 
 
 
@@ -19,12 +19,12 @@ function App() {
       <AuthProvider>
       <Router>
         <Routes>
-          <Route path='/profile' element={<EditProfile/>}/>
+          {/* <Route path='/profile' element={<EditProfile/>}/> */}
           <Route path='/login' element={<Login/>}/>
           <Route path='/users' element={<SearchPaginationUsers/>}/>
           <Route path='/' element={<Landing/>}/>
           <Route path='/signup' element={<Signup/>}/>
-          <Route path='/crud' element={<Crud/>}/>
+          {/* <Route path='/crud' element={<Crud/>}/> */}
           <Route path='*' element={<Error/>}/>
         </Routes>
       </Router></AuthProvider>
